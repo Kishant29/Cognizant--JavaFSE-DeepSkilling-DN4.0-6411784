9eac95ca9b28b5613b0c92792320cee146f82c85
Kishant K <162338451+Kishant29@users.noreply.github.com>
2025-06-29 23:35:30 +0530

    Add files via upload

---
+++ Week2_TDD using jUnit 5 and Mockito/Mockito Exercise 2 - Verifying Interactions/main/kishant/MyService (1).java
@@ -0,0 +1,15 @@
+package com.kishant;
+
+import com.external.ExternalApi;
+
+public class MyService {
+    private final ExternalApi externalApi;
+
+    public MyService(ExternalApi externalApi) {
+        this.externalApi = externalApi;
+    }
+
+    public void fetchData() {
+        externalApi.getData("kishant", 5);
+    }
+}
